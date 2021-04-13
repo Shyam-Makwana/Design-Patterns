f6dae448024e9d1994a3ae96567aa03aa3a6bd1b
Shyam-Makwana <makwanashyam6@gmail.com>
2021-04-14 01:11:54 +0530

    Added Strategy DP

---
+++ Strategy/DescendingSorting.java
@@ -0,0 +1,23 @@
+package Strategy;
+
+/*
+ * @author Shyam Makwana
+ */
+
+public class DescendingSorting implements SortingStrategy {
+
+    @Override
+    public void sort(int[] numbers) {
+        int numberCount = numbers.length;
+        int temp = 0;
+        for (int i = 0; i < numberCount - 1; i++) {
+            for (int j = i + 1; j < numberCount; j++) {
+                if (numbers[i] < numbers[j]) {
+                    temp = numbers[j];
+                    numbers[j] = numbers[i];
+                    numbers[i] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ Strategy/SortingStrategy.java
@@ -0,0 +1,9 @@
+package Strategy;
+
+/*
+ * @author Shyam Makwana
+ */
+
+public interface SortingStrategy {
+    public void sort(int[] numbers);
+}
